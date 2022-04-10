61c52c54c601aaeb049c678e8420caba102d3a2c
Arnaud <kapp.arno@gmail.com>
2022-04-10 04:11:30 +0200

    Use paho-mqtt via conan

--- conanfile.py
+++ conanfile.py
@@ -18,11 +18,11 @@ class LeosacConan(ConanFile):
                "spdlog/1.9.2", \
                "date/3.0.1", \
                "websocketpp/0.8.2", \
-               "gtest/1.11.0", \
-               "libgpiod/1.6.3"
+               "libgpiod/1.6.3"               
 
     options = {
-        'build_test': [True, False]
+        'build_test': [True, False],
+        'build_module_mqtt': [True, False]
     }
 
     generators = 'cmake'
@@ -39,10 +39,18 @@ class LeosacConan(ConanFile):
     exports_sources = "CMakeLists.txt", "cmake*", "src*", "scripts*", "cfg*", "deps*", "test*"
     keep_imports = True
 
+    def requirements(self):
+        if self.options.build_test:
+            self.requires('gtest/1.11.0')
+        if self.options.build_module_mqtt:
+            self.requires('paho-mqtt-cpp/1.2.0')
+    
     def build(self):
         cmake = CMake(self)
         if self.options.build_test:
             cmake.definitions['LEOSAC_BUILD_TESTS'] = True
+        if self.options.build_module_mqtt:
+            cmake.definitions['LEOSAC_BUILD_MODULE_MQTT'] = True
         cmake.configure()
         cmake.build()
 
